8e378cb8d3aeab174dd30dce8c3d78a375856e35
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-19 14:40:00 -0300

    relacionamento com endereço

--- src/main/java/br/org/serratec/model/Cliente.java
+++ src/main/java/br/org/serratec/model/Cliente.java
@@ -1,15 +1,15 @@
 package br.org.serratec.model;
 
 import java.util.Date;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
+import javax.persistence.OneToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -55,7 +55,7 @@ public class Cliente {
 	@NotNull(message = "Insira a data de nascimento.")
 	private Date dataNascimento;
 
-	@ManyToOne
+	@OneToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "id_endereco")
 	private Endereco endereco;
 
