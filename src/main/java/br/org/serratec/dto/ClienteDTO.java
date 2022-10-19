60c67ce1775a8b15deae497ba79d2ecdfa9b024a
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-19 10:57:40 -0300

    Cleinte DTO

--- src/main/java/br/org/serratec/dto/ClienteDTO.java
+++ src/main/java/br/org/serratec/dto/ClienteDTO.java
@@ -1,5 +1,25 @@
 package br.org.serratec.dto;
 
+import br.org.serratec.model.Cliente;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
 public class ClienteDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+    private String cpf;
+
+    public ClienteDTO(Cliente cliente) {
+        this.id = cliente.getIdCliente();
+        this.nome = cliente.getNome();
+        this.email = cliente.getEmail();
+        this.cpf = cliente.getCpf();
+    }
     
 }
