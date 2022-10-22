9e22069fc2df67afe9fe1ff42226711494b5e692
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-22 14:01:38 -0300

    atualizando clienteDTO

--- src/main/java/br/org/serratec/dto/CategoriaDTO.java
+++ src/main/java/br/org/serratec/dto/CategoriaDTO.java
@@ -9,14 +9,14 @@ import lombok.Setter;
 @Getter
 @NoArgsConstructor
 public class CategoriaDTO {
-    
-	private Long id;
+
+    private Long id;
     private String nome;
     private String descricao;
 
     public CategoriaDTO(Categoria categoria) {
-    	this.id = categoria.getIdCategoria();
-        this.nome = categoria.getNomeCategoria();
-        this.descricao = categoria.getDescricaoCategoria();
+        this.id = categoria.getId();
+        this.nome = categoria.getNome();
+        this.descricao = categoria.getDescricao();
     }
 }
