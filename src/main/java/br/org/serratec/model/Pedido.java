7e8dc0b86cefbe326ef38f99be0d060a0accb1c3
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-22 13:38:33 -0300

    passando enum do status no pedido

--- src/main/java/br/org/serratec/model/Pedido.java
+++ src/main/java/br/org/serratec/model/Pedido.java
@@ -4,16 +4,17 @@ import java.time.LocalDate;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
+import br.org.serratec.enums.Status;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -36,13 +37,13 @@ public class Pedido {
     @Column(name = "data_entrega")
     private LocalDate dataEntrega;
 
-    @NotBlank(message = "Entre com um status valido")
-    @Size(max = 20)
-    private String Status;
+    @Enumerated(EnumType.STRING)
+    private Status status;
 
     @JsonBackReference
     @ManyToOne
     @JoinColumn(name = "id_cliente")
     private Cliente cliente;
+    
 
 }
