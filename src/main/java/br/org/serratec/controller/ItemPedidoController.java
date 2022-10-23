f90ab53e314466371908fab1b7c0d2a8cd838e30
nello0517 <aniellolongo555@gmail.com>
2022-10-23 14:32:56 -0300

    atualizando o controller

--- src/main/java/br/org/serratec/controller/ItemPedidoController.java
+++ src/main/java/br/org/serratec/controller/ItemPedidoController.java
@@ -1,7 +1,7 @@
 package br.org.serratec.controller;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,42 +14,113 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.org.serratec.dto.ItemPedidoDTO;
 import br.org.serratec.service.ItemPedidoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
-@RequestMapping("/api/itempedidos")
+@RequestMapping("/itempedidos")
 public class ItemPedidoController {
 
     @Autowired
     private ItemPedidoService service;
 
     @GetMapping
+	@ApiOperation(value = "Lista todos os itens pedidos")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna todos os itens pedidos"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+			@ApiResponse(responseCode = "404", description = "Item pedido não encontrado"),
+			@ApiResponse(responseCode = "500", description = "Erro na aplicação")
+	})
 	public ResponseEntity<List<ItemPedidoDTO>> listarTodos(){		
 		return ResponseEntity.ok(service.listarTodos());
 	}
 	
-    @GetMapping("/{id}")
-	public ResponseEntity<Optional<ItemPedidoDTO>> listarPorId(@PathVariable Long id){
-		return ResponseEntity.ok(service.listarPorId(id));
-	}
+	@GetMapping("{id}")
+	@ApiOperation(value = "Lista item pedido pelo id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Retorna item pedido do id referenciado"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+			@ApiResponse(responseCode = "404", description = "Item pedido não encontrado"),
+			@ApiResponse(responseCode = "500", description = "Erro na aplicação")
+	})
+	public ResponseEntity<ItemPedidoDTO> listarPorId(@PathVariable Long id){
+		ItemPedidoDTO itemPedidoDTO = service.listarPorId(id);
 
-    @PostMapping
+        if (itemPedidoDTO != null) {
+		    return ResponseEntity.ok(service.listarPorId(id));
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+	@PostMapping
+    @ApiOperation(value = "Cadastrado uma nova item pedido", notes = "preencha com os dados da item pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Retorna o item pedido cadastrado"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "422", description = "Você credencias já cadastradas no banco de dados"),
+            @ApiResponse(responseCode = "404", description = "Item pedido  não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
 	public ResponseEntity<ItemPedidoDTO> cadastrar(@RequestBody ItemPedidoDTO itemPedido) {
-		ItemPedidoDTO dto = service.cadastrar(itemPedido);
-		return new ResponseEntity<>(dto, HttpStatus.CREATED);
+		ItemPedidoDTO itemPedidoDTO = service.cadastrar(itemPedido);
+        if (itemPedidoDTO != null) {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(itemPedidoDTO.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(itemPedidoDTO);
+        }
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 
-    @PutMapping("/{id}")
+    @PutMapping("{id}")
+	@ApiOperation(value = "Cadastrado um novo item pedido ", notes = "preencha com os dados do item pedido")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Retorna o item pedido cadastrado"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+			@ApiResponse(responseCode = "422", description = "Você credencias já cadastradas no banco de dados"),
+			@ApiResponse(responseCode = "404", description = "Item pedido  não encontrado"),
+			@ApiResponse(responseCode = "500", description = "Erro na aplicação")
+	})
 	public ResponseEntity<ItemPedidoDTO> atualizar(@PathVariable Long id, @RequestBody ItemPedidoDTO itemPedido) {
-		ItemPedidoDTO dto = service.atualizar(id, itemPedido);		
-		return ResponseEntity.ok(dto);
+		ItemPedidoDTO itemPedidoDTO = service.atualizar(id, itemPedido);		
+        if (itemPedidoDTO != null) {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(itemPedidoDTO.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(itemPedidoDTO);
+        } 
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 
-    @DeleteMapping("/{id}")
+
+    @DeleteMapping("{id}")
+    @ApiOperation(value = "Deleta um item pedido", notes = "preencha com o id do item pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deletado com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "404", description = "Item pedido não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
 	public ResponseEntity<?> apagar(@PathVariable Long id){
-		service.apagar(id);
-		return ResponseEntity.ok("O id:" + id + " foi deletado com sucesso!"); 
+		Boolean response = service.apagar(id);
+        if (response != true) {
+            return ResponseEntity.notFound().build();
+        }
+	
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
--- src/main/java/br/org/serratec/controller/PedidoController.java
+++ src/main/java/br/org/serratec/controller/PedidoController.java
@@ -1,7 +1,7 @@
 package br.org.serratec.controller;
 
+import java.net.URI;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,9 +14,14 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.org.serratec.dto.PedidoDTO;
 import br.org.serratec.service.PedidoService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
 
 @RestController
 @RequestMapping("/api/pedidos")
@@ -25,31 +30,98 @@ public class PedidoController {
     @Autowired
     private PedidoService service;
 
-    @GetMapping
+	@GetMapping
+    @ApiOperation(value = "Lista todos os pedidos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna todos os pedidos"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "404", description = "Pedido não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
 	public ResponseEntity<List<PedidoDTO>> listarTodos(){		
 		return ResponseEntity.ok(service.listarTodos());
 	}
 	
-    @GetMapping("/{id}")
-	public ResponseEntity<Optional<PedidoDTO>> listarPorId(@PathVariable Long id){
-		return ResponseEntity.ok(service.listarPorId(id));
+	@GetMapping("{id}")
+    @ApiOperation(value = "Lista pedido pelo id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Retorna pedido do id referenciado"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "404", description = "Pedido não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
+	public ResponseEntity<PedidoDTO> listarPorId(@PathVariable Long id){
+		PedidoDTO pedidoDTO = service.listarPorId(id);
+
+		if (pedidoDTO != null) {
+		    return ResponseEntity.ok(service.listarPorId(id));
+		}
+
+        return ResponseEntity.notFound().build();
 	}
 
-    @PostMapping
+	@PostMapping
+    @ApiOperation(value = "Cadastrado uma nova pedido", notes = "preencha com os dados da pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Retorna o pedido cadastrado"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "422", description = "Você credencias já cadastradas no banco de dados"),
+            @ApiResponse(responseCode = "404", description = "Pedido não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
 	public ResponseEntity<PedidoDTO> cadastrar(@RequestBody PedidoDTO pedido) {
-		PedidoDTO dto = service.cadastrar(pedido);
-		return new ResponseEntity<>(dto, HttpStatus.CREATED);
-	}
+		PedidoDTO pedidoDTO = service.cadastrar(pedido);
+		if (pedidoDTO != null) {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(pedidoDTO.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(pedidoDTO);
+        }
 
-    @PutMapping("/{id}")
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+	}
+	
+
+	@PutMapping("{id}")
+	@ApiOperation(value = "Cadastrado um novo pedido", notes = "preencha com os dados do pedido")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "201", description = "Retorna o pedido cadastrado"),
+			@ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+			@ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+			@ApiResponse(responseCode = "422", description = "Você credencias já cadastradas no banco de dados"),
+			@ApiResponse(responseCode = "404", description = "Pedido não encontrado"),
+			@ApiResponse(responseCode = "500", description = "Erro na aplicação")
+	})
 	public ResponseEntity<PedidoDTO> atualizar(@PathVariable Long id, @RequestBody PedidoDTO pedido) {
-		PedidoDTO dto = service.atualizar(id, pedido);		
-		return ResponseEntity.ok(dto);
+		PedidoDTO pedidoDTO = service.atualizar(id, pedido);		
+        if (pedidoDTO != null) {
+            URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                    .buildAndExpand(pedidoDTO.getId())
+                    .toUri();
+            return ResponseEntity.created(uri).body(pedidoDTO);
+        } 
+        
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 	}
 
-    @DeleteMapping("/{id}")
+    @DeleteMapping("{id}")
+    @ApiOperation(value = "Deleta um pedido", notes = "preencha com o id do pedido")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Deletado com sucesso"),
+            @ApiResponse(responseCode = "401", description = "Erro de autenticação"),
+            @ApiResponse(responseCode = "403", description = "Você não tem permissão para o recurso"),
+            @ApiResponse(responseCode = "404", description = "Pedido não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro na aplicação")
+    })
 	public ResponseEntity<?> apagar(@PathVariable Long id){
-		service.apagar(id);
-		return ResponseEntity.ok("O id:" + id + " foi deletado com sucesso!"); 
+		Boolean response = service.apagar(id);
+        if (response != true) {
+            return ResponseEntity.notFound().build();
+        }
+	
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 }
