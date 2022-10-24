a0f60b0833489025510cd6c5b30fe4d53e72dbdc
João Victor (kifel) <joaovictorm.pessoal@gmail.com>
2022-10-24 15:14:17 -0300

    ignored parameter types

--- src/main/java/br/org/serratec/config/SwaggerConfig.java
+++ src/main/java/br/org/serratec/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package br.org.serratec.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import br.org.serratec.model.Cliente;
+import br.org.serratec.model.Endereco;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -20,6 +22,7 @@ public class SwaggerConfig {
                 .apis(RequestHandlerSelectors.basePackage("br.org.serratec.controller"))
                 .paths(PathSelectors.any())
                 .build()
+                .ignoredParameterTypes(Cliente.class, Endereco.class)
                 .apiInfo(apiInfo());
     }
 
